5ffd3fe60f89f04d3e010a38e602aac9c3d786ae
Đức Trí <nguyentri.alan@gmail.com>
2024-06-24 11:01:23 +0700

    .

---
+++ manager.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
+
+class CheckpoinitManager:
+    def __init__(self, saved_folder: str, num_saved: int = 3) -> None:
+        self.saved_folder = saved_folder
+        self.num_saved = num_saved
+
+        self.saved_checkpoints = []
+
+    def load_checkpoint(self, checkpoint_path: str, model: nn.Module, optimizer: optim.Optimizer, scheduler: lr_scheduler.LRScheduler):
+        checkpoint = torch.load(checkpoint_path, map_location='cpu')
+
+        model.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        scheduler.load_state_dict(checkpoint['scheduler'])
+
+        n_steps = checkpoint['n_steps']
+        n_epochs = checkpoint['n_epochs']
+
+        return n_steps, n_epochs
+    
+    def save_checkpoint(self, model: nn.Module, optimizer: optim.Optimizer, scheduler: lr_scheduler.LRScheduler, n_steps: int, n_epochs: int):
+        data = {
+            'model': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'scheduler': scheduler.state_dict(),
+            'n_steps': n_steps,
+            'n_epochs': n_epochs
+        }
+
+        if len(self.saved_checkpoints) == self.num_saved:
+            shutil.rmtree(f"{self.saved_folder}/{self.saved_checkpoints[0]}.pt")
+            self.saved_checkpoints.pop(0)
+
+        torch.save(data, f"{self.saved_folder}/{n_steps}.pt")
+
+        self.saved_checkpoints(n_steps)
--- train.py
+++ train.py
@@ -16,8 +16,10 @@ from .model.vit import ViT
 from .processing.processor import ViTProcessor
 from .dataset import ViTCollate, ViTDataset
 from .evaluation import ViTCriterion
+from .manager import CheckpoinitManager
 
 from tqdm import tqdm
+import fire
 from typing import Optional
 
 def setup(rank: int, world_size: int):
@@ -38,6 +40,10 @@ def train(rank: int,
           num_train_samples: Optional[int] = None,
           lr: float = 7e-5,
           fp16: bool = True,
+          # Checkpoint Config
+          checkpoint: Optional[str] = None,
+          saved_checkpoints: str = "./checkpoints",
+          save_checkpoint_after: int = 1,
           # Data Val Config
           val_path: Optional[str] = None,
           val_batch_size: int = 1,
@@ -45,7 +51,7 @@ def train(rank: int,
           # Processor Config
           input_size: int = 64,
           label_path: str = "./configs/cifar10.json",
-          #
+          # Model Config
           patch_size: int = 16,
           n_layers: int = 12, 
           d_model: int = 768, 
@@ -53,6 +59,14 @@ def train(rank: int,
           activation: str = 'gelu', 
           dropout_p: float = 0.1
         ):
+    if rank == 0:
+        if os.path.exists(saved_checkpoints) == False:
+            os.makedirs(saved_checkpoints)
+
+        checkpoint_manager = CheckpoinitManager(saved_checkpoints)
+        n_steps = 0
+        n_epochs = 0
+
     processor = ViTProcessor(input_size, label_path)
 
     model = ViT(
@@ -70,6 +84,10 @@ def train(rank: int,
     optimizer = optim.Adam(params=model.parameters(), lr=lr)
     scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=1000)
 
+    if checkpoint is not None:
+        assert os.path.exists(checkpoint), "NOT FOUND CHECKPOINT"
+        n_steps, n_epochs = checkpoint_manager.load_checkpoint(checkpoint, model, optimizer, scheduler)
+
     if world_size > 1:
         model = DDP(model, device_ids=[rank])
 
@@ -85,7 +103,7 @@ def train(rank: int,
     for epoch in range(num_epochs):
         train_losses = []
 
-        for index, (x, y) in enumerate(tqdm(train_dataloader)):
+        for (x, y) in tqdm(train_dataloader):
             with autocast(enabled=fp16):
                 outputs = model(x)
             loss = criterion.cross_entropy_loss(outputs, y)
@@ -96,6 +114,67 @@ def train(rank: int,
             scaler.update()
 
             train_losses.append(loss.item())
-        
-        
 
+            n_steps += 1
+
+        n_epochs += 1
+
+        if rank == 0:
+            if epoch % save_checkpoint_after == save_checkpoint_after - 1 or epoch == num_epochs - 1:
+                checkpoint_manager.save_checkpoint(model, optimizer, scheduler, n_steps, n_epochs)
+
+    if world_size > 1:
+        cleanup()
+
+def main(
+        # Data Train Config
+          train_path: str,
+          num_epochs: int = 1,
+          train_batch_size: int = 1,
+          num_train_samples: Optional[int] = None,
+          lr: float = 7e-5,
+          fp16: bool = True,
+          # Checkpoint Config
+          checkpoint: Optional[str] = None,
+          saved_checkpoints: str = "./checkpoints",
+          save_checkpoint_after: int = 1,
+          # Data Val Config
+          val_path: Optional[str] = None,
+          val_batch_size: int = 1,
+          num_val_samples: Optional[int] = None,
+          # Processor Config
+          input_size: int = 64,
+          label_path: str = "./configs/cifar10.json",
+          # Model Config
+          patch_size: int = 16,
+          n_layers: int = 12, 
+          d_model: int = 768, 
+          n_heads: int = 12, 
+          activation: str = 'gelu', 
+          dropout_p: float = 0.1
+        ):
+    n_gpus = torch.cuda.device_count()
+
+    if n_gpus == 1:
+        train(
+            0, n_gpus, train_path, num_epochs, train_batch_size, num_train_samples, lr, bool(fp16),
+            checkpoint, saved_checkpoints, save_checkpoint_after,
+            val_path, val_batch_size, num_val_samples,
+            input_size, label_path,
+            patch_size, n_layers, d_model, n_heads, activation, dropout_p
+        )
+    elif n_gpus > 1:
+        mp.spawn(
+            train,
+            args=(
+                n_gpus, train_path, num_epochs, train_batch_size, num_train_samples, lr, bool(fp16),
+                checkpoint, saved_checkpoints, save_checkpoint_after,
+                val_path, val_batch_size, num_val_samples,
+                input_size, label_path,
+                patch_size, n_layers, d_model, n_heads, activation, dropout_p
+            ),
+            nprocs=n_gpus,
+            join=True
+        )
+    else:
+        print("NOT SUPPORT CPU")
