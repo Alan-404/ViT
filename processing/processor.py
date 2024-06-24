e35cc25774c7a7348d1fc6f1cd8bb039ea081902
Đức Trí <nguyentri.alan@gmail.com>
2024-06-24 10:15:00 +0700

    setup dataset and configs

--- processing/processor.py
+++ processing/processor.py
@@ -1,21 +1,29 @@
 import torch
 from torchvision import transforms
-import cv2 as cv
+import json
 from PIL import Image
-from typing import List, Union
+from typing import List, Union, Dict, Tuple
 import numpy as np
 
 class ViTProcessor:
     def __init__(self,
                  input_size: Union[List[int], int],
-                 labels: List[str]) -> None:
+                 labels: Union[Dict[str, int], str]) -> None:
         self.input_size = (input_size, input_size) if isinstance(input_size, int) else input_size
 
         self.resize = transforms.Resize(self.input_size)
         self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
-        self.labels = labels
+        if isinstance(labels, str):
+            labels = self.load_label(labels)
+        
+        self.labels = list(labels.keys())
+        self.label_indexes = list(labels.values())
     
+    def load_label(self, path: str):
+        with open(path, 'r') as file:
+            return json.load(file)
+
     def load_image(self, path: str) -> torch.Tensor:
         image = Image.open(path).convert('RGB')
         image = self.resize(image)
